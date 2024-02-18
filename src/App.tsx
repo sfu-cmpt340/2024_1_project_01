91541745592cfd603382d5c00f219e53b7617dcb
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-02-17 23:20:05 -0800

    Added inputs to upload image and display it in App component
    -Added text box to enter url for image
    -Added button to fetch image from url
    -Displays image after submitting url
    -Added button to remove image to allow user to get another image from a url

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,57 @@
 import { useState } from 'react'
 
 const App: React.FC = () => {
+    const [image, setImage] = useState<string>('');
+    const [imageURL, setImageURL] = useState<string>('');
+    
     const [data, setData] = useState<JSON | null>(null);
 
     return (
       <>
-        <button onClick={() => {
-            fetch('/classify').then(
-                async (res: Response) => setData(await res.json())
-            );
-        }}>
-            test classify call
-        </button>
-        <p>
-            classify return data: {data && (data as any)['data']}
-        </p>
+        {image 
+            ? <div>
+                <img src={image} /> 
+                <button 
+                    onClick={() => setImage('')}
+                >
+                    remove image
+                </button>
+            </div>
+            : <div>
+                <div>
+                    <input 
+                        type="url"
+                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setImageURL(e.target.value);
+                        }}
+                    />
+                    <button 
+                        onClick={async () => {
+                            const res: Response = await fetch(imageURL);
+                            const blob: Blob = await res.blob();
+                            const objectURL: string = URL.createObjectURL(blob);
+                            setImage(objectURL);
+                        }}
+                    >
+                        submit
+                    </button>
+                    <img src={image} />
+                </div>
+            </div>
+        }
+        <div>
+            <button 
+                onClick={() => {
+                    fetch('/classify')
+                        .then(async (res: Response) => setData(await res.json()));
+                }}
+            >
+                classify
+            </button>
+            <p>
+                result from classify: {data && (data as any)['data']}
+            </p>
+        </div>
       </>
     );
 }
