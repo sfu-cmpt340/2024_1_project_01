5ce4312bb63c15fe46431934b54264a911bd2c21
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-02-18 21:23:48 -0800

    Setup absolute imports
    -Installed types/node dependency
    -Added absolute import alias using @

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,11 @@ export default defineConfig({
   server: {
     proxy: {
         '/classify': 'http://localhost:5000'
+    }
+  },
+  resolve: {
+    alias: {
+        "@": resolve(__dirname, './src')
     }
   }
 })
