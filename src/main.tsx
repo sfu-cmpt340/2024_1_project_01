d58e3c3419ceb7fb1f6f5e2d2123ed618bf95336
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-02-18 18:57:36 -0800

    Setup react router for website
    -Installed react router package
    -Created and set up router in Main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+
 import App from './App.tsx'
 
+const router: Router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />
+    }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
