a09be4cd0ff36f8d8ef0527e44fc6697cfed6cd9
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-03-03 19:00:17 -0800

    Updated main component
    -Set up Mantine and Tailwind in main component
    -Updated react router paths to include future previous diagnoses, diagnosis, about, and error pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createTheme, MantineProvider } from '@mantine/core'
+
+import '@mantine/core/styles.css'
+import '@/styles.css'
 
 import Classifier from '@/pages/classifier/index'
 import Home from '@/pages/home/index'
+
+const theme = createTheme({});
 
 const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
     {
@@ -13,11 +19,26 @@ const router: ReturnType<typeof createBrowserRouter> = createBrowserRouter([
     {
         path: '/classifier',
         element: <Classifier />
+    },
+    {
+        path: '/previous-diagnoses'
+    },
+    {
+        path: '/diagnosis/:datetime'
+    },
+    {
+        path: '/about'
+    },
+    {
+        path: '*',
+        element: <p>404 Error</p>
     }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <MantineProvider theme={theme}>
+            <RouterProvider router={router} />
+        </MantineProvider>
     </React.StrictMode>
 );
