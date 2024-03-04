ace5a999d394477b150607de029e5c0f3483cb83
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-03-03 19:05:15 -0800

    Recreated Figma design for Home page
    -Added hero image to public folder
    -Implemented Figma design

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,57 @@
+import { Button, Overlay, Progress } from '@mantine/core'
 import { Link } from 'react-router-dom'
-
 
 // DESC: Home page for website; displays information about classifer.
 const Home: React.FC = () => {
     return (
         <>
-            <Link to={'/classifier'}> 
-                <button>
-                    use classfier
-                </button>
-            </Link>
-            <Link to={'/'}>
-                <button>
-                    previous results
-                </button>
-            </Link>
+            <div className="w-screen h-screen absolute z-0">
+                <Overlay 
+                    gradient="linear-gradient(90deg, rgba(28,28,28,1) 0%, rgba(255,255,255,0) 100%)"
+                />
+                <img className="w-screen h-screen" src="/hero.jpg" />
+            </div>
+            <div className="w-screen h-screen flex items-center">
+                <div className="flex flex-col gap-8 font-Inter relative z-10 m-20">
+                    <p className="font-black text-white text-8xl">
+                        Skintector
+                    </p>
+                    <Progress className="w-5/6" color="#3943B7" value={100} />
+                    <p className="font-bold text-white text-2xl">
+                        Diagnose skin conditions from the comfort of your own home
+                    </p>
+                    <div className="flex gap-12">
+                        <Button
+                            className="w-[30%]"
+                            variant="filled" 
+                            color="#3943B7"
+                            size="lg" 
+                            radius="md" 
+                            component={Link} 
+                            to="/classifier"
+                        >
+                            <p className="text-white text-2xl font-bold">
+                                Get Started
+                            </p>
+                        </Button>
+                        <Button
+                            className="w-[30%]"
+                            variant="filled"
+                            color="#CED3DC"
+                            size="lg"
+                            radius="md"
+                            ps="10"
+                            pe="10"
+                            component={Link}
+                            to="/previous-diagnoses" 
+                        >
+                            <p className="text-black text-2xl font-bold">
+                                View Diagnoses
+                            </p>
+                        </Button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
