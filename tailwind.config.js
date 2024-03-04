feb20dad447e6b51445cbef035867911ef822ae1
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-03-03 18:58:53 -0800

    Set up Mantine and Tailwind
    -Installed Mantine and Tailwind packages
    -Installed Inter black, bold, and regular fonts
    -Set up postcss and tailwind css files
    -Created styles.css file and linked tailwind and fonts together

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'Inter': ['Inter', 'serif']
+            },
+            colors: {
+                'blue': '#3943B7',
+                'white': '#FCF7F8',
+                'black': '#1C1C1C',
+                'aqua': '#006BA6',
+                'grey': '#CED3DC',
+                'red': '#DB5461'
+            }
+        }
+    },
+    plugins: [],
+}
+
