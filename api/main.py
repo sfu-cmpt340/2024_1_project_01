7a7f8f58a4ee3d0e423ac385b55de755233c2db1
longhtr <longtranhai98@gmail.com>
2024-03-17 10:09:16 -0700

    Updated main.py to use Tensorflow serving model - Faster loading time - Faster inference - No more weird model loading behaviour (before first prediction takes much longer than subsequent predictions)
    Updated requirements.txt to require only tensorflow (no keras and jax)
    - Every dependencies are explicitly listed as before

--- api/main.py
+++ api/main.py
@@ -1,24 +1,23 @@
-from base64 import b64decode
-from flask import Flask, request
-import io
-import jax
 import json
-import keras
-from keras.preprocessing.image import img_to_array, load_img
+from base64 import b64decode
+
+import tensorflow as tf
+from flask import Flask, request
 
 app = Flask(__name__)
 
 # Load EfficientNetV2M trained on SD-198
 print("Loading model...")
-model = keras.saving.load_model("./api/model.keras")
+serving_model = tf.saved_model.load("./api/model")
 print("Model loaded!")
 
-# Load labels 
+# Load labels
 print("Loading labels...")
 labels = None
 with open("./api/labels.txt", "r") as file:
     labels = [line for line in file]
 print("Labels loaded!")
+
 
 @app.route("/classify", methods=["POST"])
 def classify() -> json:
@@ -29,26 +28,25 @@ def classify() -> json:
 
     # Decode image data and convert to PIL Image
     image_bytes = b64decode(image_data.split(",")[1])
-    image = load_img(io.BytesIO(image_bytes), target_size=(480, 480))
-
-    # Preprocess image for the model
-    image = img_to_array(image)
-    image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+    image = tf.image.decode_image(image_bytes)
+    image = tf.image.resize(image, (480, 480))
+    image = tf.expand_dims(image, 0)
 
     # Predict the class
     print("Predicting class...")
-    predictions = model.predict(image)
+    predictions = serving_model.serve(image).numpy().flatten()
 
     # Sort and save top 5 conditions
-    sorted_predictions = sorted(
-        zip(predictions[0], labels), key=lambda x: x[0], reverse=True
-    )[:5]
-    top_predictions = {str(label): prob * 100 for prob, label in sorted_predictions}
+    sorted_indices = predictions.argsort()[::-1][:5]
+    top_predictions = {
+        str(labels[i]): float(predictions[i] * 100) for i in sorted_indices
+    }
 
     print("Predictions made!")
     print(top_predictions)
 
     return top_predictions
+
 
 if __name__ == "__main__":
     app.run()
