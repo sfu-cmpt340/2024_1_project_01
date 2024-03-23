24d371f3f8297758acc5e1023172f43c8937c249
jeffreyjkjin <jinjeffrey24@gmail.com>
2024-03-22 17:53:09 -0700

    Updated ConditionCard component
    -Installed swr package
    -Implemented logic to dynamically load condition information from json files and display to user
    -Replicated Figma design for condition card information

--- src/pages/diagnosis/ConditionCard.tsx
+++ src/pages/diagnosis/ConditionCard.tsx
@@ -1,14 +1,85 @@
+import useSWR from 'swr'
+
 interface ConditionCardProps {
     probability: number;
     condition: string;
 }
 
-// DESC: Generates a card with information about the condition.
-//  PRE: condition - A json file containing its condition information exists.
+//  DESC: Generates a card with information about the condition.
+//   PRE: A json file exists containing the information on the provided condition.
+// PARAM: probability - Probability of the condition that may be present in the user's image.
+//        condition - Name of the condition that may be present in the user's image.
 const ConditionCard: React.FC<ConditionCardProps> = ({ probability, condition }) => {
+    // fetch information about condition
+    const fetcher = (path: string) => fetch(path).then((res) => res.json());
+    const { data, error, isLoading } = useSWR(`/condition_data/${condition}.json`, fetcher)
+
+    // load lists
+    const possibleCauses: JSX.Element[] = !(isLoading || error) && data['possible causes'].map((cause: string) => {
+        return (
+            <li key={condition + cause}>
+                {cause}
+            </li>
+        );
+    });
+
+    const symptoms: JSX.Element[] = !(isLoading || error) && data['symptoms'].map((symptom: string) => {
+        return (
+            <li key={condition + symptom}>
+                {symptom}
+            </li>
+        );
+    });
+
+    const treatments: JSX.Element[] = !(isLoading || error) && data['treatments'].map((treatment: string) => {
+        return (
+            <li key={condition + treatment}>
+                {treatment}
+            </li>
+        );
+    });
+
     return (
-        <div className="font-bold">
-            {condition.replace(/_/g, ' ')} - {probability.toFixed(2)}%
+        <div className="flex flex-col font-Inter">
+            <p className="text-xl font-bold">
+                {condition.replace(/_/g, ' ')} - {probability.toFixed(2)}%
+            </p>
+            <div className="flex gap-8">
+                {!error
+                    ? <div className="flex flex-col">
+                        <p className="text-justify">
+                            {!isLoading && data['description']}
+                        </p>
+                        <div className="my-4">
+                            <p className="text-md font-bold">
+                                Possible Causes
+                            </p>
+                            <ul className="list-disc list-inside">
+                                {possibleCauses}
+                            </ul>
+                            <p className="text-md font-bold">
+                                Symptoms
+                            </p>
+                            <ul className="list-disc list-inside">
+                                {symptoms}
+                            </ul>
+                            <p className="text-md font-bold">
+                                Treatments
+                            </p>
+                            <ul className="list-disc list-inside">
+                                {treatments}
+                            </ul>
+                        </div>
+                    </div>
+                    : <div>
+                        Information on this condition could not be loaded.
+                    </div>
+                }
+                <img 
+                    className="shrink-0 w-[320px] h-[240px] ml-auto" 
+                    src={`/condition_images/${condition}.jpg`} 
+                />
+            </div>
         </div>
     );
 }
